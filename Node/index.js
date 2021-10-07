a33be2825efd558708dd2ff4c887aaa41afd8689
anthonydominguez86 <adominguez11186@gmail.com>
2021-10-06 21:02:57 -0400

    running some node

--- Node/index.js
+++ Node/index.js
@@ -8,8 +8,13 @@ console.log ("You are" + age + " years old");
 
 function myColor (color) {
     console.log ("Your favorite color is" + color);
-    }
+}
+
+function myKids (kids) {
+    console.log ("You have " + kids + " children");
+}
 
 sayHi(" Anthony")
-myAge(" 35")
+myAge (" 35")
 myColor (" orange")
+myKids ("3")

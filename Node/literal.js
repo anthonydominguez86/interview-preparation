65dc48a6fb0082c25a8b21f6cd41644df897b27c
anthonydominguez86 <adominguez11186@gmail.com>
2021-11-02 18:28:26 -0400

    template literal practice

---
+++ Node/literal.js
@@ -0,0 +1,13 @@
+const arya = {
+    first: "Arya",
+    last: "Stark",
+    origin: "Winterfell",
+    allegiance: "House Stark"
+  };
+  
+  const greeting = `My name is ${arya.first}!
+  I am from ${arya.origin}.`;
+  
+  console.log(greeting); // prints
+  // My name is Arya!
+  // I am loyal to House Stark.

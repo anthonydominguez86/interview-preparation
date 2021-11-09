5b7f7d9de23e61bd769765c6ec268a60c6b0935c
anthonydominguez86 <adominguez11186@gmail.com>
2021-11-08 19:26:37 -0500

    math.js added for practice

---
+++ Node/Math Sum/maths.js
@@ -0,0 +1,15 @@
+module.exports = {
+    sum: function(a, b){
+      return a + b;
+    },
+    difference: function(a, b){
+      return a - b;
+    },
+    product: function(a, b){
+      return a * b;
+    },
+    quotient: function(a, b){
+      return a / b;
+    }
+  };
+  
